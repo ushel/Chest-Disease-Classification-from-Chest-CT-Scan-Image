503a6074cf64f0c6b440c5ab4821e6930efdf6f3
ushel <utkarsh.shelke03@gmail.com>
2024-10-28 13:25:39 +0530

    Data ingestion notebook

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -27,7 +27,7 @@ def read_yaml(path_to_yaml:Path) -> ConfigBox:
         ConfigBox: ConfigBox type
     """
     try:
-        with open(path_to_yaml, 'r') as yaml_file:
+        with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file: {path_to_yaml} loaded successfully.")
             return ConfigBox(content)
