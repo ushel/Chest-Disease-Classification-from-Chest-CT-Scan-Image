2d407e88a6d5f60bc81b0fd805efbd23beb24add
ushel <utkarsh.shelke03@gmail.com>
2024-10-29 22:55:32 +0530

    Model evaluation pipeline completed

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -71,18 +71,7 @@ class ConfigurationManager:
         )
 
         return training_config
-    
-class ConfigurationManager:
-    def __init__(
-        self,
-        config_filepath = CONFIG_FILE_PATH,
-        params_filepath = PARAMS_FILE_PATH):
-        
-        self.config = read_yaml(config_filepath)
-        self.params = read_yaml(params_filepath)
-        
-        create_directories([self.config.artifacts_root])
-        
+
     def get_evaluation_config(self) -> EvaluationConfig:
         eval_config = EvaluationConfig(
             path_of_model="artifacts/training/model.h5",
